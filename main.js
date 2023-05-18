69b2b733f3b16b4d3d31a2769f6b146e18d8a614
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-05-19 00:40:14 +0300

    add GET routes

--- main.js
+++ main.js
@@ -3,6 +3,31 @@ const app = express()
 const port = process.env.PORT || 3000
 const router = express.Router()
 
-app.use(express.static(__dirname + '/'))
-app.use("/", router)
+// app.use(express.static(__dirname + '/films'))
+// app.use("/films", router)
+
+// получаем данные для всех фильмов
+app.get("/films", function(request, response) {
+
+    response.sendFile('index.html', {root: __dirname })
+})
+
+// получаем все сеансы для выбранного фильма
+app.get("/:movieName/seances", function(request, response){
+     
+    response.send("<h1>Seances</h1>");
+});
+ 
+// получаем места на сеансе
+app.get("/:movieName/:seanceCinema/:seanceDate/places", function(request, response){
+     
+    response.send("<h1>Places</h1>");
+});
+ 
+// получаем данные корзины
+app.get("/cart", function(request, response){
+     
+    response.send("<h1>Cart</h1>");
+});
+
 app.listen(port);
