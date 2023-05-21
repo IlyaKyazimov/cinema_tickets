3b7bc9afe29ab0f4f33e6e4dcdfffb1a2628eb8e
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-05-21 20:32:53 +0300

    create seances component

---
+++ front-end/src/app/seances/seances.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './seances.component.html',
+  styleUrls: ['./seances.component.css']
+})
+export class SeancesComponent {
+  title = 'front-end';
+}
