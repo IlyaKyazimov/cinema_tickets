ee972e80a749c674de237c7f70cf497fddf134b6
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-05-21 20:33:12 +0300

    create places component

---
+++ front-end/src/app/places/places.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './places.component.html',
+  styleUrls: ['./places.component.css']
+})
+export class PlacesComponent {
+  title = 'front-end';
+}
