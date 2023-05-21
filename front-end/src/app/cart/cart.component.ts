fb93b9c2d569670cfa61609b17369a9dea2a39d8
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-05-21 20:34:03 +0300

    create cart component

---
+++ front-end/src/app/cart/cart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {
+  title = 'front-end';
+}
