59f90d0614a335965e0b5f7b2f11ca31efe550fe
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-05-21 20:31:59 +0300

    create film component

---
+++ front-end/src/app/films/films.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './films.component.html',
+  styleUrls: ['./films.component.css']
+})
+export class FilmsComponent {
+  title = 'front-end';
+}
