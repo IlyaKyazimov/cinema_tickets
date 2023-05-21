fa0af537bbfcf47bf06b796e32324e3a8d9770ff
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-05-21 20:34:40 +0300

    change main page

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,22 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
-import { HeaderComponent } from './header/header.component';
+import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+
+import {Routes, RouterModule} from '@angular/router';
+
+import { FilmsComponent } from './films/films.component';
+import { SeancesComponent } from './seances/seances.component';
+import { PlacesComponent } from './places/places.component';
+import { CartComponent } from './cart/cart.component';
+
+const appRoutes: Routes = [
+  { path: 'films', component: FilmsComponent},
+  { path: ':movieName/seances', component: SeancesComponent},
+  { path: ':movieName/:seanceCinema/:seanceDate/places', component: PlacesComponent},
+  { path: 'cart', component: CartComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FilmsComponent,
+    SeancesComponent,
+    PlacesComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent, HeaderComponent, FooterComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
