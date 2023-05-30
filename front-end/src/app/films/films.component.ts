fa1cc7fc73ec30e2de7ccad9105dbe337125365d
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-05-30 22:17:51 +0300

    fetch data for films component

--- front-end/src/app/films/films.component.ts
+++ front-end/src/app/films/films.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-films',
   templateUrl: './films.component.html',
   styleUrls: ['./films.component.css']
 })
-export class FilmsComponent {
+export class FilmsComponent implements OnInit {
+
+  movies: any;
+    constructor(private http: HttpClient) { }
+
+    ngOnInit() {
+        this.http.get('http://localhost:3000/films').subscribe({next:(data:any) => this.movies = data}); // console.log(data)
+    }
 }
