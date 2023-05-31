61c4e97a6d642a274f1e4e50dd0ab4a93dd6fb4a
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-05-31 22:47:41 +0300

    add getImage function

--- front-end/src/app/films/films.component.ts
+++ front-end/src/app/films/films.component.ts
@@ -7,10 +7,11 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./films.component.css']
 })
 export class FilmsComponent implements OnInit {
-
   movies: any;
     constructor(private http: HttpClient) { }
 
+    getImage = (movieName: string) => movieName.replace(/\*| |:|%|#|&|\$/g,'');
+    
     ngOnInit() {
         this.http.get('http://localhost:3000/films').subscribe({next:(data:any) => this.movies = data}); // console.log(data)
     }
