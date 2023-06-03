2358ad04573d6be307f8881f453a449f6237186c
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-06-03 19:05:25 +0300

    add date

--- front-end/src/app/films/films.component.ts
+++ front-end/src/app/films/films.component.ts
@@ -7,12 +7,15 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./films.component.css']
 })
 export class FilmsComponent implements OnInit {
-  movies: any;
-    constructor(private http: HttpClient) { }
 
-    getImage = (movieName: string) => movieName.replace(/\*| |:|%|#|&|\$/g,'');
+  date: any = new Date().toLocaleString("default", { year: "numeric" }) + "-" + new Date().toLocaleString("default", { month: "2-digit" }) + "-" + new Date().toLocaleString("default", { day: "2-digit" });
+  movies: any;
+  
+  constructor(private http: HttpClient) { }
+
+  getImage = (movieName: string) => movieName.replace(/\*| |:|%|#|&|\$/g,'');
     
-    ngOnInit() {
-        this.http.get('http://localhost:3000/films').subscribe({next:(data:any) => this.movies = data}); // console.log(data)
-    }
+  ngOnInit() {
+    this.http.get('http://localhost:3000/films').subscribe({next:(data:any) => this.movies = data}); // console.log(data)
+  }
 }
