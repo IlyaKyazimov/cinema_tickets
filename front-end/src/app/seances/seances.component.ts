bb634dd9edc3d1e0dc154a12bd56586407b1c72f
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-06-03 18:55:45 +0300

    add fetch data from server

--- front-end/src/app/seances/seances.component.ts
+++ front-end/src/app/seances/seances.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-seances',
   templateUrl: './seances.component.html',
   styleUrls: ['./seances.component.css']
 })
-export class SeancesComponent {
-  title = 'front-end';
+export class SeancesComponent implements OnInit {
+
+  seancesData: any;
+  movieName: string;
+  seanceDate: string;
+
+  constructor(private http: HttpClient, private activateRoute: ActivatedRoute) { 
+
+    this.movieName = activateRoute.snapshot.params['movieName'];
+    this.seanceDate = activateRoute.snapshot.params['seanceDate'];
+  }
+
+  getImage = () => this.movieName?.replace(/\*| |:|%|#|&|\$/g,'');
+
+  ngOnInit() {
+    this.http.get('http://localhost:3000/' + this.movieName + '/' + this.seanceDate + '/seances').subscribe({ next: (data: any) => this.seancesData = data }); // console.log(data)
+  }
 }
