d169df2178aa989ed5b3e7857c1faca5177dfdef
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-06-08 21:07:27 +0300

    update PlacesInfo and Place changes

--- front-end/src/app/places/places.component.ts
+++ front-end/src/app/places/places.component.ts
@@ -10,14 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 export class PlacesComponent {
 
   seatsRows: any;
+  placesInfo: any;
   movieName: string;
   seanceDate: string;
   seanceCinema: string;
   cinemaAddress!: string;
-  count: number = 0;
+  reservedAmount: number = 0;
   price!: number;
-  totalSum: number = 0;
+  totalSum!: number;
   seanceTime: string;
+
+  receivedData: any;
 
   constructor(private http: HttpClient, private activateRoute: ActivatedRoute) {
 
@@ -31,27 +34,64 @@ export class PlacesComponent {
 
   formatSeanceTime = (time: string) => time.slice(0, -3);
 
-  getPrice() {
-    this.count++;
-    this.totalSum = this.count * this.price;
+  getPrice(seat: any) {
+    const status = seat.status == 'Free' ? 'Reserved' : 'Free';
+
+    if (status == 'Free') {
+      this.reservedAmount--;
+      this.placesInfo.busy--;
+      this.placesInfo.free++;
+    } else {
+      this.reservedAmount++;
+      this.placesInfo.busy++;
+      this.placesInfo.free--;
+    }
+
+    this.totalSum = this.reservedAmount * this.price;
+
+    this.postPlace(seat, status, this.placesInfo).subscribe({
+      next: (data: any) => { this.receivedData = data; },
+      error: error => console.log(error)
+    });
+
+    seat.status = status;
+  }
+
+  postPlace(seat: any, status: string, placesInfo: any) {
+
+    const body = { seatId: seat.id, seatStatus: status, placesInfoId: placesInfo.id, placesInfoFree: placesInfo.free, placesInfoBusy: placesInfo.busy };
+    return this.http.post('http://localhost:3000/' + this.movieName + '/' +
+      this.seanceDate + '/' + this.seanceCinema + '/' + this.seanceTime + '/places', body);
   }
 
   ngOnInit() {
-    this.http.get('http://localhost:3000/' + this.movieName + '/' + this.seanceDate + '/' + this.seanceCinema + '/' + this.seanceTime + '/places').subscribe({
-      next: (data: any) => {
 
-        this.seatsRows = data[0].places.reduce(function (arr: { [x: string]: any[]; }, obj: { line: string | number; }) {
-          arr[obj.line] = arr[obj.line] || [];
-          arr[obj.line].push(obj);
-          return arr;
-        }, []);
+    this.http.get('http://localhost:3000/' + this.movieName + '/' + this.seanceDate + '/' +
+      this.seanceCinema + '/' + this.seanceTime + '/places').subscribe({
+        next: (data: any) => {
 
-        this.seatsRows = this.seatsRows.slice(1);
-        this.cinemaAddress = data[0].seance.cinema.address;
-        this.price = data[0].seance.price;
+          this.seatsRows = data[0].places.reduce(function (arr: { [x: string]: any[]; }, obj: { line: string | number; }) {
+            arr[obj.line] = arr[obj.line] || [];
+            arr[obj.line].push(obj);
+            return arr;
+          }, []);
 
-        return;
-      }
-    }); // console.log(data)
+          this.seatsRows = this.seatsRows.slice(1);
+
+          for (let seatsRow of this.seatsRows) {
+            for (let seat of seatsRow) {
+              if (seat.status == 'Reserved') this.reservedAmount++;
+            }
+          }
+
+          this.placesInfo = { id: data[0].id, free: data[0].free, busy: data[0].busy };
+          this.cinemaAddress = data[0].seance.cinema.address;
+          this.price = data[0].seance.price;
+
+          this.reservedAmount > 0 ? this.totalSum = this.price * this.reservedAmount : this.totalSum = 0;
+
+          return;
+        }
+      });
   }
 }
