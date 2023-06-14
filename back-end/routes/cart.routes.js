edc2027b35f97d0ec14b89964a65c67b929b853d
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-06-14 17:36:25 +0300

    add routes for getting and canceling orders

--- back-end/routes/cart.routes.js
+++ back-end/routes/cart.routes.js
@@ -4,4 +4,8 @@ module.exports = app => {
 
     // Creat Order
     app.post("/cart", controller.createOrder);
+    // Get orders
+    app.get("/cart", controller.getOrders);
+    // Cancel orders
+    app.put("/cart", controller.cancelOrders);
 };
