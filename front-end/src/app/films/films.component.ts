28f6f45e0d214eaf47944f2b78093265887b4e0a
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-06-15 01:17:52 +0300

    add getToday and getTomorrow functions

--- front-end/src/app/films/films.component.ts
+++ front-end/src/app/films/films.component.ts
@@ -8,14 +8,33 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FilmsComponent implements OnInit {
 
-  date: any = new Date().toLocaleString("default", { year: "numeric" }) + "-" + new Date().toLocaleString("default", { month: "2-digit" }) + "-" + new Date().toLocaleString("default", { day: "2-digit" });
+  date: any;
   movies: any;
-  
+
   constructor(private http: HttpClient) { }
 
-  getImage = (movieName: string) => movieName.replace(/\*| |:|%|#|&|\$/g,'');
-    
+  getImage = (movieName: string) => movieName.replace(/\*| |:|%|#|&|\$/g, '');
+
+  getToday() {
+    this.date = new Date().toLocaleString("default", { year: "numeric" }) + "-" +
+         new Date().toLocaleString("default", { month: "2-digit" }) + "-" + 
+         new Date().toLocaleString("default", { day: "2-digit" });
+  }
+
+  getTomorrow() {
+    this.date = new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toLocaleString("default", { year: "numeric" }) + "-" +
+         new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toLocaleString("default", { month: "2-digit" }) + "-" + 
+         new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toLocaleString("default", { day: "2-digit" });
+  }
+
   ngOnInit() {
-    this.http.get('http://localhost:3000/films').subscribe({next:(data:any) => this.movies = data}); // console.log(data)
+    this.http.get('http://localhost:3000/films').subscribe({
+      next: (data: any) => {
+        this.movies = data;
+        this.date = new Date().toLocaleString("default", { year: "numeric" }) + "-" +
+         new Date().toLocaleString("default", { month: "2-digit" }) + "-" + 
+         new Date().toLocaleString("default", { day: "2-digit" });
+      }
+    });
   }
 }
