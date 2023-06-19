1304d1662dafe1b00b2653bafc1ef2db5b373c19
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-06-19 14:42:50 +0300

    add methods and variables for filter usage

--- front-end/src/app/seances/seances.component.ts
+++ front-end/src/app/seances/seances.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-seances',
@@ -10,20 +10,52 @@ import { ActivatedRoute } from '@angular/router';
 export class SeancesComponent implements OnInit {
 
   cinemasData: any;
+  cinemasNames: any;
   movieName: string;
   seanceDate: string;
+  checkedCinemas: string[] = [];
 
-  constructor(private http: HttpClient, private activateRoute: ActivatedRoute) { 
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { 
 
-    this.movieName = activateRoute.snapshot.params['movieName'];
-    this.seanceDate = activateRoute.snapshot.params['seanceDate'];
+    this.movieName = route.snapshot.params['movieName'];
+    this.seanceDate = route.snapshot.params['seanceDate'];
   }
 
   formatTitle = (title: string) => title?.replace(/\*| |:|%|#|&|\$/g,'');
 
   formatSeanceTime = (time: string) => time.slice(0, -3);
 
+  getCheckedCinemas(value: string) {
+    if (this.checkedCinemas.includes(value)) {
+      this.checkedCinemas = this.checkedCinemas.filter((item) => item !== value);
+    } else {
+      this.checkedCinemas.push(value);
+    }
+  }
+
+  trackByIdentifier(index: number, item: any): number { return item.id }
+
+  applyFilter() { this.ngOnInit() }
+
+  cancelFilter() { window.location.reload() }
+
   ngOnInit() {
-    this.http.get('http://localhost:3000/' + this.movieName + '/' + this.seanceDate + '/seances').subscribe({ next: (data: any) => this.cinemasData = data }); // console.log(data)
+
+    this.router.navigate([this.movieName, this.seanceDate, 'seances'], {
+      queryParams: {
+        cinemaName: this.checkedCinemas
+      }
+    })
+      .then(() => {
+        const queryParams = this.route.snapshot.queryParams;
+        const routeWithParams = this.router.createUrlTree([], { queryParams }).toString();
+
+        this.http.get('http://localhost:3000' + routeWithParams).subscribe({
+          next: (data: any) => { 
+            this.cinemasData  = data[0];
+            this.cinemasNames = data[1];
+           }
+        });
+      })
   }
 }
