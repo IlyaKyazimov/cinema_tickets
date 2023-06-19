9876bdcd9f307eb71be834dd7382bc2216f593a7
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-06-19 13:35:02 +0300

    add conditions for using filter

--- back-end/controllers/movie.controller.js
+++ back-end/controllers/movie.controller.js
@@ -1,12 +1,27 @@
 const db = require("../models");
 const Movie = db.movie;
+const Country = db.country;
 
 exports.getMovies = (req, res) => {
-    Movie.findAll({
-        include: [{
-            model: db.country,
-            attributes: ["name"]
-        }],
+    let movieConditions = {};
+    req.query.genre ? movieConditions.genre = req.query.genre : null;
+    req.query.ageRating ? movieConditions.ageRating = req.query.ageRating + '+' : null;
+
+    let countryCondition = req.query.country ? { name: req.query.country } : null;
+
+    db.sequelize.transaction((transaction) => {
+        return Promise.all([
+            Movie.findAll({
+                where: movieConditions,
+                include: [{
+                    model: db.country,
+                    where: countryCondition,
+                    attributes: ["name"]
+                }],
+            }, { transaction }),
+            Country.findAll({ attributes: ["name"] }, { transaction }),
+            Movie.rawAttributes.genre.values
+        ]);
     })
         .then(data => {
             console.log(data);
