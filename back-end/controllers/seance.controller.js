5b29c14bcb19355054fd26100a7daee013ee49dd
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-06-19 14:41:29 +0300

    add condition for filter usage

--- back-end/controllers/seance.controller.js
+++ back-end/controllers/seance.controller.js
@@ -2,26 +2,34 @@ const db = require("../models");
 const Cinema = db.cinema;
 
 exports.getCinemas = (req, res) => {
-    Cinema.findAll({
-        include: [{
-            model: db.seance,
-            where: { date: new Date(req.params.seanceDate) },
-            attributes: ["date", "time", "price"],
-            include: [{
-                model: db.movie,
-                where: { name: req.params.movieName },
-                attributes: ["name", "description"]
-            }]
-        }]
+    let cinemaCondition = req.query.cinemaName ? { name: req.query.cinemaName } : null;
+
+    db.sequelize.transaction((transaction) => {
+        return Promise.all([
+            Cinema.findAll({
+                where: cinemaCondition,
+                include: [{
+                    model: db.seance,
+                    where: { date: new Date(req.params.seanceDate) },
+                    attributes: ["date", "time", "price"],
+                    include: [{
+                        model: db.movie,
+                        where: { name: req.params.movieName },
+                        attributes: ["name", "description"]
+                    }]
+                }]
+            }, { transaction }),
+            Cinema.findAll({ attributes: ["name"] }, { transaction })
+        ]);
     })
-    .then(data => {
-        console.log(data);
-        res.status(200).send(data);
-    })
-    .catch(err => {
-        res.status(500).send({
-            message:
-                err.message || "Some error occurred while retrieving films."
+        .then(data => {
+            console.log(data);
+            res.status(200).send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving films."
+            });
         });
-    });
 };
