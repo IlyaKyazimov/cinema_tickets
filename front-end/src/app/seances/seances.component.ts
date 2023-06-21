ba239e75a301a3e91fa34ad98e1af85e7913f105
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-06-22 00:09:10 +0300

    add filter for date, design changes

--- front-end/src/app/seances/seances.component.ts
+++ front-end/src/app/seances/seances.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-seances',
@@ -10,18 +11,102 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class SeancesComponent implements OnInit {
 
   cinemasData: any;
+  description: any;
   cinemasNames: any;
   movieName: string;
   seanceDate: string;
   checkedCinemas: string[] = [];
 
-  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { 
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute, public sanitizer: DomSanitizer) {
 
     this.movieName = route.snapshot.params['movieName'];
     this.seanceDate = route.snapshot.params['seanceDate'];
   }
 
-  formatTitle = (title: string) => title?.replace(/\*| |:|%|#|&|\$/g,'');
+  createUrl(movieName: string, seanceDate: string, cinemaName: string, seanceTime: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(movieName + '/' + seanceDate + '/' +
+      cinemaName + '/' + this.formatSeanceTime(seanceTime) + '/places');
+  }
+
+  formatTitle = (title: string) => title?.replace(/\*| |:|%|#|&|\$/g, '');
+
+  getDate() {
+
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      if (checkbox instanceof HTMLInputElement) {
+        checkbox.checked = false;
+      }
+    });
+    this.checkedCinemas = [];
+
+    this.router.navigate([this.movieName, this.seanceDate, 'seances'])
+      .then(() => {
+
+        this.http.get('http://localhost:3000/' + this.movieName + '/' + this.seanceDate + '/seances').subscribe({
+          next: (data: any) => {
+
+            this.cinemasData = data[0];
+            this.cinemasNames = data[1];
+            this.description = this.cinemasData[0].seances[0].movie.description;
+          }
+        });
+      })
+  }
+
+  getToday() {
+    this.seanceDate = new Date().toLocaleString("default", { year: "numeric" }) + "-" +
+      new Date().toLocaleString("default", { month: "2-digit" }) + "-" +
+      new Date().toLocaleString("default", { day: "2-digit" });
+
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      if (checkbox instanceof HTMLInputElement) {
+        checkbox.checked = false;
+      }
+    });
+    this.checkedCinemas = [];
+
+    this.router.navigate([this.movieName, this.seanceDate, 'seances'])
+      .then(() => {
+
+        this.http.get('http://localhost:3000/' + this.movieName + '/' + this.seanceDate + '/seances').subscribe({
+          next: (data: any) => {
+
+            this.cinemasData = data[0];
+            this.cinemasNames = data[1];
+            this.description = this.cinemasData[0].seances[0].movie.description;
+          }
+        });
+      })
+  }
+
+  getTomorrow() {
+    this.seanceDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toLocaleString("default", { year: "numeric" }) + "-" +
+      new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toLocaleString("default", { month: "2-digit" }) + "-" +
+      new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toLocaleString("default", { day: "2-digit" });
+
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      if (checkbox instanceof HTMLInputElement) {
+        checkbox.checked = false;
+      }
+    });
+    this.checkedCinemas = [];
+
+    this.router.navigate([this.movieName, this.seanceDate, 'seances'])
+      .then(() => {
+
+        this.http.get('http://localhost:3000/' + this.movieName + '/' + this.seanceDate + '/seances').subscribe({
+          next: (data: any) => {
+
+            this.cinemasData = data[0];
+            this.cinemasNames = data[1];
+            this.description = this.cinemasData[0].seances[0].movie.description;
+          }
+        });
+      })
+  }
 
   formatSeanceTime = (time: string) => time.slice(0, -3);
 
@@ -37,7 +122,30 @@ export class SeancesComponent implements OnInit {
 
   applyFilter() { this.ngOnInit() }
 
-  cancelFilter() { window.location.reload() }
+  cancelFilter() {
+    //window.location.reload()
+
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      if (checkbox instanceof HTMLInputElement) {
+        checkbox.checked = false;
+      }
+    });
+    this.checkedCinemas = [];
+
+    this.router.navigate([this.movieName, this.seanceDate, 'seances'])
+      .then(() => {
+
+        this.http.get('http://localhost:3000/' + this.movieName + '/' + this.seanceDate + '/seances').subscribe({
+          next: (data: any) => {
+
+            this.cinemasData = data[0];
+            this.cinemasNames = data[1];
+            this.description = this.cinemasData[0].seances[0].movie.description;
+          }
+        });
+      })
+  }
 
   ngOnInit() {
 
@@ -51,10 +159,11 @@ export class SeancesComponent implements OnInit {
         const routeWithParams = this.router.createUrlTree([], { queryParams }).toString();
 
         this.http.get('http://localhost:3000' + routeWithParams).subscribe({
-          next: (data: any) => { 
-            this.cinemasData  = data[0];
+          next: (data: any) => {
+            this.cinemasData = data[0];
             this.cinemasNames = data[1];
-           }
+            this.description = this.cinemasData[0].seances[0].movie.description;
+          }
         });
       })
   }
