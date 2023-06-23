ceae8acdfc31e418dd1ebc65de8ae477bfd24cde
Ilya Kyazimov <mister.kyazimov@gmail.com>
2023-06-23 12:06:54 +0300

    configurations change

--- back-end/config/db.config.js
+++ back-end/config/db.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "11235813",
-    DB: "cinema_tickets",
+    HOST: process.env.DB_HOST || 'localhost',
+    USER: process.env.DB_USER || 'root',
+    PASSWORD: process.env.DB_PASSWORD || "11235813",
+    DB: process.env.DB_NAME || "cinema_tickets",
+    PORT: process.env.DB_PORT || 3306,
     dialect: "mysql"
   };
